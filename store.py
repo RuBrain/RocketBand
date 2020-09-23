3e9c030f734dfe8dc9e6db90ff5ef09c31f334db
Danil Osipov <danko11rus@gmail.com>
2020-09-23 22:00:24 +0300

    feat: 1-9 item done

---
+++ store.py
@@ -0,0 +1,44 @@
+data = [
+{
+"first_name": "Nikolay",
+"Last_name": "Ivanov",
+"age": 43,
+"gender": "M",
+"last_salary_up_day": "2016/01/31"
+},
+{
+"first_name": "Alexander",
+"Last_name": "Petrov",
+"age": 25,
+"gender": "M",
+"last_salary_up_day": "2018/04/31"
+},
+{
+"first_name": "Olga",
+"Last_name": "Davidova",
+"age": 34,
+"gender": "F",
+"last_salary_up_day": "2012/02/31"
+},
+{
+"first_name": "Irina",
+"Last_name": "Siroshtan",
+"age": 20,
+"gender": "F",
+"last_salary_up_day": "2015/07/31"
+},
+{
+"first_name": "Dmitry",
+"Last_name": "Sava",
+"age": 27,
+"gender": "M",
+"last_salary_up_day": "2020/03/31"
+},
+{
+"first_name": "Dmitry",
+"Last_name": "Sava",
+"age": 33,
+"gender": "M",
+"last_salary_up_day": "2020/05/31"
+}
+]
